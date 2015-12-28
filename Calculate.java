dd34bbe14f77f80a0e0632703fff0b2d4168c377
Shahin Shamiyev <Shahin Shamiyev>
2015-12-28 13:31:01 +0400

    user input

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,9 @@
-public class Calculate {
+public class calculate {
     public static void main(String[] arg){
         System.out.println("Calculate");
+		int a = Integer.valueof(arg[0]);
+		int b = Integer.valueof(arg[1]);
+		int sum = a + b;
+		System.out.println(sum);
     }
 }
